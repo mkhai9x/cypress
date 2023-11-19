64b9593a03b13e0d85f7897268bc1e03710b5447
Minh Tran <minh.tran.unsw@gmail.com>
2023-11-20 01:18:06 +1100

    set up landing page

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const HomePageLayout = () => {
-  return (
-    <div>HomePageLayout</div>
-  )
-}
+const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
+  return <main>{children}</main>;
+};
 
-export default HomePageLayout
+export default HomePageLayout;
